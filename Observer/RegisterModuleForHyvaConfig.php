7c3f3951494e90d6149bed81d15ec5abf729649c
Sean van Zuidam <sean.grimlink@gmail.com>
2023-06-16 12:04:15 +0200

    Update RegisterModuleForHyvaConfig.php

--- Observer/RegisterModuleForHyvaConfig.php
+++ Observer/RegisterModuleForHyvaConfig.php
@@ -8,7 +8,7 @@ use Magento\Framework\Event\ObserverInterface;
 
 class RegisterModuleForHyvaConfig implements ObserverInterface
 {
-    private $componentRegistrar;
+    private ComponentRegistrar $componentRegistrar;
 
     public function __construct(ComponentRegistrar $componentRegistrar)
     {
@@ -20,9 +20,7 @@ class RegisterModuleForHyvaConfig implements ObserverInterface
         $config = $event->getData('config');
         $extensions = $config->hasData('extensions') ? $config->getData('extensions') : [];
 
-        $moduleName = implode('_', array_slice(explode('\\', __CLASS__), 0, 2));
-
-        $path = $this->componentRegistrar->getPath(ComponentRegistrar::MODULE, $moduleName);
+        $path = $this->componentRegistrar->getPath(ComponentRegistrar::MODULE, '<VENDOR>_<MODULE>');
 
         // Only use the path relative to the Magento base dir
         $extensions[] = ['src' => substr($path, strlen(BP) + 1)];
